031af26d3a6888f63fb2a9ce96273ed379b39aec
Pradeep Buddharaju <pradeep.buddharaju@gmail.com>
2021-02-05 10:28:45 -0600

    Created the Xcode project

---
+++ SampleAppSOAPI/SampleAppSOAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleAppSOAPI
+//
+//  Created by Pradeep on 2/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
